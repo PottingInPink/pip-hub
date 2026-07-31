5ea7e208b53dd55c183a081cf602cbb1131d5df5
PottingInPink <pottinginpink@gmail.com>
2026-07-31 14:05:04 -0400

    Create glazeCatalog.js

---
+++ src/glazeCatalog.js
@@ -0,0 +1,30 @@
+export const GLAZE_BRANDS_WITH_SERIES = {
+    Amaco: ['PC', 'Flux', 'Celadons', 'Matte', 'Cosmos', 'Crackle', 'Float', 'Velvets', 'LUGs', 'Palettes', 'Other'],
+    Mayco: ['Stroke and Coat', 'Stoneware', 'Matte', 'Flux', 'Raku', 'Other'],
+    Spectrum: ['Floating', 'Raku', 'Other']
+}
+
+export const GLAZE_BRANDS_NO_SERIES = [
+    'Coyote',
+    'Georgies',
+    'Laguna',
+    'Opulence',
+    'Speedball',
+    'Western',
+    'Wicked',
+    'Other'
+  ]
+
+export const ALL_GLAZE_BRANDS = [
+    'Amaco',
+    'Coyote',
+    'Georgies',
+    'Laguna',
+    'Mayco',
+    'Opulence',
+    'Spectrum',
+    'Speedball',
+    'Western',
+    'Wicked',
+    'Other'
+  ]
