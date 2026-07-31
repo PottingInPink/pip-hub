a580c0611f67ec903f8ed36d648bcfec9d6c5fe2
PottingInPink <pottinginpink@gmail.com>
2026-07-31 09:46:26 -0400

    Create productCatalog.js

---
+++ src/productCatalog.js
@@ -0,0 +1,89 @@
+export const PRODUCT_CATALOG = {
+  'Plant Hooks': {
+    aliases: ['plant hook', 'plant hooks', 'plant say what', 'hook', 'hooks'],
+    variantLabel: 'Phrase',
+    variants: ['Hearts', 'Michigan', 'Mushrooms', 'Flowers']
+  },
+  'Wall Flowers': {
+    aliases: ['wall flower', 'wall flowers', 'wallflower', 'wallflowers'],
+    variantLabel: 'Color',
+    variants: [],
+    sizes: ['XL', 'L', 'M', 'S', 'XS']
+  },
+  'Sticks': {
+    aliases: ['stick', 'sticks'],
+    variantLabel: 'Design',
+    variants: ['Flower', 'Tree', 'Sea Critter', 'Chickens', 'Cats', 'Other', 'Mushroom', 'Morel', 'Heart', 'Poppy', 'Penis', 'Pottery']
+  },
+  'Garden Crones': {
+    aliases: ['garden crone', 'garden crones'],
+    variantLabel: 'Color',
+    variants: [],
+    hasPattern: true
+  },
+  'Bowls': {
+    aliases: ['bowl', 'bowls'],
+    variantLabel: 'Style',
+    variants: ['Ruffle', 'Large', 'Small']
+  },
+  'Trays': {
+    aliases: ['tray', 'trays'],
+    variantLabel: 'Style',
+    variants: ['Crackle', 'Large', 'Small']
+  },
+  'Minis': {
+    aliases: ['mini', 'minis'],
+    variantLabel: 'Type',
+    variants: ['Functional', 'Critter', 'Food', 'Mushroom', 'Pumpkin', 'Silly']
+  },
+  'Whimsies': {
+    aliases: ['whimsy', 'whimsies'],
+    variantLabel: 'Kind',
+    variants: []
+  },
+  'Mugs': {
+    aliases: ['mug', 'mugs'],
+    variantLabel: 'Style',
+    variants: ['Carved', 'Plain']
+  },
+  'Tumblers': {
+    aliases: ['tumbler', 'tumblers'],
+    variantLabel: 'Style',
+    variants: ['Phrases', 'Plain']
+  },
+  'Ornaments': {
+    aliases: ['ornament', 'ornaments'],
+    variantLabel: 'Design',
+    variants: ['Cat', 'Michigan', 'Star', 'Tree', 'Heart', 'Acorn', 'Pinecone', 'Boy', 'Angel', 'Snowflake', 'Other']
+  },
+  'Spoon Rests': {
+    aliases: ['spoon rest', 'spoon rests'],
+    variantLabel: 'Style',
+    variants: ['Plain', 'Grater']
+  },
+  'Tiles': {
+    aliases: ['tile', 'tiles'],
+    variantLabel: 'Design',
+    variants: ['Lady', 'Rat', 'Possum', 'Chicken', 'Racoon', 'Birds', 'Other']
+  }
+}
+
+export const SIMPLE_PRODUCTS = [
+  'Med Tray',
+  'Incense Burner',
+  'Vulva',
+  'Worry Stone',
+  'Stepping Stones',
+  'Neuro Nuggets',
+  'Bookmarks',
+  'Jewelry',
+  'Magnets',
+  'Hearts',
+  'Trees',
+  'Wiseguys',
+  'Muddy Buddys',
+  'Rock Hoards',
+  'Bee Blossoms',
+  'Mushroom Stakes',
+  'Ashtrays'
+  ]
